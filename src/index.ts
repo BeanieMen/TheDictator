02f80f18184973c2bc7d236e21e015ad1feb1b7f
Beanie <62057938+BeanieMen@users.noreply.github.com>
2024-10-17 10:06:01 +0530

    add shit

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,10 @@
-import axios from "axios";
-import { load } from "cheerio";
 import { Command } from "commander";
+import { scrapeTables } from "./scrape";
 import { analyzeConjugation } from "./irregularities";
 
 const program = new Command();
 
-const scrapeTables = async (verb: string) => {
-  try {
-    const { data } = await axios.get(
-      `https://www.spanishdict.com/conjugate/${verb}`
-    );
-    const $ = load(data);
 
-    const tablesData: string[][][] = [];
-
-    $("table").each((index, table) => {
-      const tableData: string[][] = [];
-
-      $(table)
-        .find("tr")
-        .each((i, row) => {
-          const rowData: string[] = [];
-
-          $(row)
-            .find("td, th")
-            .each((j, cell) => {
-              rowData.push($(cell).text().trim());
-            });
-
-          if (rowData.length > 0) {
-            tableData.push(rowData);
-          }
-        });
-
-      tablesData.push(tableData);
-    });
-
-    return {
-      indicative: tablesData[1] || [],
-      subjunctive: tablesData[2] || [],
-    };
-  } catch (error) {
-    console.error("Error scraping tables:", error);
-    return { indicative: [], subjunctive: [] };
-  }
-};
 
 const main = async (verb: string) => {
   const verbConj = await scrapeTables(verb);
